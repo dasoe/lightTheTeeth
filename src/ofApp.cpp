8fa056073962d7606d4598ab77efcde00e7682e1
das oe <christian.oettinger@gmx.de>
2021-11-02 16:49:39 +0100

    drawing added

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,13 +5,13 @@ void ofApp::setup(){
 	settings.addColor("beige", ofColor(10, 23, 33, 4));
 
 	for (int i = 0; i < 7; i++) {
-		settings.addVec2f("position" + i, ofVec2f(i*100, 100));
+		settings.addVec2f("position" + ofToString(i), ofVec2f(i*100, 100));
 	}
 
 	settings.init("settings.xml", true);
 
 	for (int i = 0; i < 7; i++) {
-		position.push_back(settings.getVec2fValue("position"+i) );
+		position.push_back(settings.getVec2fValue("position"+ ofToString(i)) );
 	}
 
 	indexToSet = 0;
@@ -25,7 +25,11 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	for (int i = 0; i < 7; i++) {
+		ofDrawRectangle( ofRectangle(position[i].x, position[i].y, 70,200) );
 
+		
+	}
 }
 
 //--------------------------------------------------------------
@@ -56,7 +60,24 @@ void ofApp::keyPressed(int key){
 	case 'h':
 		showHelp = !showHelp;
 		break;
+	case 'q':
+		position[indexToSet].x += 10;
+		settings.setVec2fValue("position" + ofToString(indexToSet), ofVec2f(position[indexToSet].x, position[indexToSet].y), true);
+		break;
+	case 'Q':
+		position[indexToSet].x -= 10;
+		settings.setVec2fValue("position" + ofToString(indexToSet), ofVec2f(position[indexToSet].x, position[indexToSet].y), true);
+		break;
+	case 'w':
+		position[indexToSet].y += 10;
+		settings.setVec2fValue("position" + ofToString(indexToSet), ofVec2f(position[indexToSet].x, position[indexToSet].y), true);
+		break;
+	case 'W':
+		position[indexToSet].y -= 10;
+		settings.setVec2fValue("position" + ofToString(indexToSet), ofVec2f(position[indexToSet].x, position[indexToSet].y), true);
+		break;
 
 	}
+
 
 }
